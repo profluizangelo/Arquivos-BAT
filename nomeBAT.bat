e7925379db233e631c9cf83919a60ec3eba027f8
Luiz Angelo de Oliveira <profluiz.angelo@gmail.com>
2019-09-17 22:13:05 -0300

    Add files via upload

---
+++ nomeBAT.bat
@@ -0,0 +1,9 @@
+@echo off
+set /p nome=Informe seu nome:
+set /p apelido=Informe seu apelido:
+set /p pais=Informe nome dos seus pais:
+echo.
+echo.
+echo Seu nome eh %nome% %apelido% e voce eh filho de %pais%
+pause
+exit
